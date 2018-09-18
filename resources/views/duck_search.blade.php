ef99d51a2b2b15a7efe3cebb63b87668ded953af
AhmedHelalAhmed <ahmed.helal.online@gmail.com>
2018-09-18 22:34:27 +0200

    add simple html page

---
+++ resources/views/duck_search.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Dan's disco Duck Page</title>
+</head>
+<body>
+<h1>Dan's disco Duck Page</h1>
+<h2>ddddddddddddddddd</h2>
+</body>
+</html>
