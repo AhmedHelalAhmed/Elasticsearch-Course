533bdd09d6463a1ce25871a4c8e3cae11235a8e1
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2018-09-19 01:05:05 +0200

    add advanced search page

--- app/Http/Controllers/DuckController.php
+++ app/Http/Controllers/DuckController.php
@@ -21,4 +21,34 @@ class DuckController extends Controller
 
         }
     }
+
+    public function advanced()
+    {
+        if($query = \Request::get('query'))
+        {
+            $duck_name = \Request::get('duck_name');
+            $duck_age= \Request::get('duck_age');
+            $duck_color=\Request::get('duck_color');
+
+            $results=Duck::search('query')->paginate(3);
+
+            if($duck_name){
+                $results = $results->where('name',$duck_name);
+            }
+            if($duck_age){
+                $results = $results->where('name',$duck_age);
+            }
+            if($duck_color){
+                $results = $results->where('name',$duck_color);
+            }
+
+            $results = $results->paginate(3);
+            return view('duck_advanced_search',['query'=>$query,'results'=>$results,'page'=>\Request::get('page') ?: 1]);
+        }
+        else
+        {
+            return view('duck_advanced_search',['query'=>null]);
+
+        }
+    }
 }
---
+++ resources/views/duck_advanced_search.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Dan's disco Duck Page</title>
+</head>
+<body>
+<h1>Dan's disco Duck Page</h1>
+<h2>Feel free to search for your favorite duck!</h2>
+{{ Form::open(['method'=>'get','route'=>'duck_search']) }}
+Duck Query: {{ Form::text('query',$query) }} <br />
+Duck Age: {{ Form::text('duck_age', $age?? '') }} <br />
+Duck Color: {{ Form::text('duck_color', $color ?? '') }} <br />
+Duck Name: {{ Form::text('duck_name', $name ?? '') }} <br />
+{{ Form::submit('Search All Ducks') }}
+{{ Form::close() }}
+@if(isset($results))
+    <h2>Results ({{ 1+ (3 * ($page -1)) }} - {{ (3*$page) > $results->total() ? $results->total() : (15 * $page) }})</h2>
+    <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Color</th>
+            <th>Gender</th>
+            <th>hometown</th>
+            <th>funky?</th>
+            <th>About</th>
+            <th>Registered Date</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($results as $result)
+            <tr>
+                <td>{{ $result->name }}</td>
+                <td>{{ $result->age }}</td>
+                <td>{{ $result->color }}</td>
+                <td>{{ $result->gender }}</td>
+                <td>{{ $result->hometown }}</td>
+                <td>{{ $result->funkyDuck }}</td>
+                <td>{{ $result->about }}</td>
+                <td>{{ $result->registered }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+    {{ $results->appends(Request::capture()->except('page'))->links() }}
+@endif
+</body>
+</html>
